4d79007d37461af811a01c6eebdb7f6825e6fa1d
Maxiee <maxieewong@gmail.com>
2014-08-15 13:14:23 +0800

    add a sidebar

--- index.php
+++ index.php
@@ -1,22 +1,31 @@
 <?php get_header(); ?>
 
-    <?php
-        query_posts('posts_per_page=10');
-            while(have_posts()) : the_post(); ?>
+    <div class = "row">
+        <div class = "col-xs-8">
+        <?php
+            query_posts('posts_per_page=10');
+                while(have_posts()) : the_post(); ?>
 
-            <div class = "container">
-                <h2><a href = "<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                <p class = "postmetadata">
-                    <?php _e('Category&#58;'); ?>
-                    <?php the_category(','); ?>
-                    <?php _e('Author&#58;'); ?>
-                    <?php the_author(','); ?>
-                    <?php edit_post_link('Edit',' | '); ?><br />
-                    <?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?>
-                </p>
-                <p><?php the_excerpt(); ?></p>
+                    <h2><a href = "<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <p class = "postmetadata">
+                        <?php _e('Category&#58;'); ?>
+                        <?php the_category(','); ?>
+                        <?php _e('Author&#58;'); ?>
+                        <?php the_author(','); ?>
+                        <?php edit_post_link('Edit',' | '); ?><br />
+                        <?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?>
+                    </p>
+                    <p><?php the_excerpt(); ?></p>
+
+                <?php endwhile; wp_reset_query(); ?>
+            <div class="navigation">
+                <?php posts_nav_link(); ?>
             </div>
+        </div>
 
-    <?php endwhile; wp_reset_query(); ?>
+        <?php get_sidebar(); ?>
+    </div>
+
+</div>
 
 <?php get_footer(); ?>
---
+++ sidebar.php
@@ -0,0 +1,9 @@
+<div class = "col-xs-4">
+    <ul>
+        <li><h2><?php _e('Categories'); ?></h2>
+            <ul>
+                <?php wp_list_cats('sort_column=name&optioncount=1&hierarchical=0'); ?>
+            </ul>
+        </li>
+    </ul>
+</div>
