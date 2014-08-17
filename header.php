e1e6d064b16234de311b1acef7e62abb8d697b6d
Maxiee <maxieewong@gmail.com>
2014-08-17 18:12:03 +0800

    header.php: show title in a better way

--- header.php
+++ header.php
@@ -1,7 +1,19 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title><?php wp_title(' | ', true, 'right'); ?></title>
+        <title><?php if ( is_home() ) {
+                bloginfo('name'); echo " - "; bloginfo('description');
+            } elseif ( is_category() ) {
+                single_cat_title(); echo " - "; bloginfo('name');
+            } elseif (is_single() || is_page() ) {
+                single_post_title();
+            } elseif (is_search() ) {
+                echo "¿?¿?¿?¿?"; echo " - "; bloginfo('name');
+            } elseif (is_404() ) {
+                echo '¿?¿?¿?¿?¿?!';
+            } else {
+                wp_title('',true);
+            } ?></title>        
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
