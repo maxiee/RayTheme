7ed8f659c7cbb5b2f91683da2624fcbe0da35265
Maxiee <maxieewong@gmail.com>
2014-08-24 14:41:44 +0800

    refactoring sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,11 +1,13 @@
-<div class = "col-sm-4">
+<div class = "col-sm-3">
     <div class="panel panel-default panel-body">
     <div class="list-group">
         <?php
-            $args=array('orderby' => 'name','order' => 'ASC');
+            $args=array('orderby' => 'id', 'order' => 'ASC', 'parent' => 0);
             $categories=get_categories($args);
             foreach($categories as $category) { 
-                echo '<a href="' . get_category_link( $category->term_id ) . '" title="' . sprintf( __( "View all posts in %s" ), $category->name ) . '" class="list-group-item"' . '><b>' . $category->name.'</b></a>';
+                echo '<a href="' . get_category_link( $category->term_id ) . '" title="' . sprintf( __( "View all posts in %s" ), $category->name ) 
+                    . '" class="list-group-item"' . '><b>' . $category->name 
+                    .' ('.$category->count.')</b></a>';
             } ?>
     </div>
     </div>
