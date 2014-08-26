6292b0b3d183d83d42cbc848e31b64f662871c32
Maxiee <maxieewong@gmail.com>
2014-08-26 16:45:25 +0800

    add weiboshow

--- sidebar.php
+++ sidebar.php
@@ -1,14 +1,17 @@
 <div class = "col-sm-3">
-    <div class="panel panel-default panel-body">
-    <div class="list-group">
-        <?php
-            $args=array('orderby' => 'id', 'order' => 'ASC', 'parent' => 0);
-            $categories=get_categories($args);
-            foreach($categories as $category) { 
-                echo '<a href="' . get_category_link( $category->term_id ) . '" title="' . sprintf( __( "View all posts in %s" ), $category->name ) 
-                    . '" class="list-group-item"' . '><b>' . $category->name 
-                    .' ('.$category->count.')</b></a>';
-            } ?>
+    <div>
+        <iframe width="100%" height="500" class="share_self"  frameborder="0" scrolling="no" src="http://widget.weibo.com/weiboshow/index.php?language=&width=0&height=500&fansRow=1&ptype=1&speed=0&skin=5&isTitle=1&noborder=1&isWeibo=1&isFans=0&uid=1240212845&verifier=8704b6ca&colors=d6f3f7,ffffff,666666,0082cb,ecfbfd&dpc=1"></iframe>
     </div>
+    <div class="panel panel-default panel-body">
+        <div class="list-group">
+            <?php
+                $args=array('orderby' => 'id', 'order' => 'ASC', 'parent' => 0);
+                $categories=get_categories($args);
+                foreach($categories as $category) { 
+                    echo '<a href="' . get_category_link( $category->term_id ) . '" title="' . sprintf( __( "View all posts in %s" ), $category->name ) 
+                        . '" class="list-group-item"' . '><b>' . $category->name 
+                        .' ('.$category->count.')</b></a>';
+                } ?>
+        </div>
     </div>
 </div>
