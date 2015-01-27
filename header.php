19f09e829d4b013675555b823724291f1efbf2ea
Maxiee <maxieewong@gmail.com>
2015-01-27 19:15:04 +0800

    header.php: fix format and fix navbar

--- header.php
+++ header.php
@@ -18,22 +18,24 @@
 	} ?></title>        
 <?php wp_head(); ?>
 </head>
-<body>
-	<nav class = "navbar navbar-default navbar-fixed-top">
-			<div class = "navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class = "navbar-brand" href="<?php bloginfo('url'); ?>" ><?php bloginfo('name'); ?></a>
-			</div>
-			<div>
-				<ul class = "nav navbar-nav">
-					<li><a href="http://www.judymax.com/%e5%85%b3%e4%ba%8e">关于</a></li>
-					<li><a href="http://www.judymax.com/%e5%bd%92%e6%a1%a3">归档</a></li>
-		<li><a href="<?php bloginfo('rss2_url'); ?>" title="RSS订阅本站">RSS订阅</a></li>
-			   </ul>
-			</div>
+
+<body <?php body_class(); ?>>
+	<nav class = "navbar navbar-default" role="navigation">
+	<div class = "navbar-header">
+		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+		<a class = "navbar-brand" href="<?php bloginfo('url'); ?>" ><?php bloginfo('name'); ?></a>
+	</div>
+	<div class="navbar-collapse collapse" id=".navbar-responsive-collapse">
+		<ul class = "nav navbar-nav">
+			<li><a href="http://www.judymax.com/%e5%85%b3%e4%ba%8e">关于</a></li>
+			<li><a href="http://www.judymax.com/%e5%bd%92%e6%a1%a3">归档</a></li>
+			<li><a href="<?php bloginfo('rss2_url'); ?>" title="RSS订阅本站">RSS订阅</a></li>
+	   </ul>
+	</div>
 	</nav>
+	
 	<div class = "container">
