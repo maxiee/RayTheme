a9e3e7fd2063b38447e138993910bc0be39d3a76
Maxiee <maxieewong@gmail.com>
2015-01-27 21:03:29 +0800

    sidebar.php: update sidebar.php to fit new theme

--- sidebar.php
+++ sidebar.php
@@ -1,19 +1,20 @@
-<div class = "col-sm-4">
-    <div>
-       <iframe width="100%" height="140" class="share_self"  frameborder="0" scrolling="no" src="http://widget.weibo.com/weiboshow/index.php?language=&width=0&height=550&fansRow=2&ptype=1&speed=0&skin=1&isTitle=1&noborder=1&isWeibo=0&isFans=0&uid=1240212845&verifier=8704b6ca&dpc=1"></iframe> 
+<div class = "col-md-3 col-lg-3">
+    <div class="panel panel-material-teal">
+	<div class="panel-heading">欢迎关注</div>
+       <iframe width="100%" height="80" class="share_self"  frameborder="0" scrolling="no" src="http://widget.weibo.com/weiboshow/index.php?language=&width=0&height=550&fansRow=2&ptype=1&speed=0&skin=1&isTitle=0&noborder=0&isWeibo=0&isFans=0&uid=1240212845&verifier=8704b6ca&colors=009688,ffffff,009688,009688,ecfbfd&dpc=1"></iframe>
     </div>
-    <div class="panel panel-default">
+    <div class="panel panel-material-teal">
         <div class="panel-heading">最多阅读</div>
         <div class="panel-body list-group">
             <?php get_most_viewed(); ?>
         </div>
     </div> 
-    <div class="panel panel-default">
+    <div class="panel panel-material-teal">
         <div class="panel-heading">专题</div>
         <div class="panel-body list-group">
             <a href="http://www.judymax.com/sympy%E6%95%99%E7%A8%8B" class="list-group-item">SymPy教程</a></div>
     </div>
-    <div class="panel panel-default">
+    <div class="panel panel-material-teal">
         <div class="panel-heading">分类</div>
         <div class="panel-body list-group">
             <?php
@@ -26,15 +27,17 @@
                 } ?>
         </div>
     </div>
-    <div class="panel">
-	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-	<!-- 大侧边栏 -->
-	<ins class="adsbygoogle"
-	     style="display:inline-block;width:336px;height:280px"
-	     data-ad-client="ca-pub-8746515866560942"
-	     data-ad-slot="6851626009"></ins>
-	<script>
-	(adsbygoogle = window.adsbygoogle || []).push({});
-	</script>
+    <div class="panel panel-material-teal">
+		<div class="panel-heading">广告</div>
+		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<!-- 响应式侧边栏 -->
+		<ins class="adsbygoogle"
+			 style="display:block"
+			 data-ad-client="ca-pub-8746515866560942"
+			 data-ad-slot="6858804406"
+			 data-ad-format="auto"></ins>
+		<script>
+		(adsbygoogle = window.adsbygoogle || []).push({});
+		</script>
     </div>
 </div>
