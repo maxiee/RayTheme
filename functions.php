48e1e9c2c38cb36f839d77be94631a700dbd757c
Maxiee <maxieewong@gmail.com>
2015-01-28 20:14:35 +0800

    add my css style.css

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ function load_style_and_scripts() {
 	wp_enqueue_style('ripples-styles', 'http://cdn.bootcss.com/bootstrap-material-design/0.2.1/css/ripples.min.css');
 	wp_enqueue_style('material-styles', 'http://cdn.bootcss.com/bootstrap-material-design/0.2.1/css/material-wfont.min.css');
 	//wp_enqueue_style('my-style', ***);
+	wp_enqueue_style('my-style', get_template_directory_uri() . '/style.css');
 	wp_enqueue_script( 'bootstrap-js', 'http://cdn.bootcss.com/bootstrap/3.3.2/js/bootstrap.min.js');
 	wp_enqueue_script( 'ripples-js', 'http://cdn.bootcss.com/bootstrap-material-design/0.2.1/js/ripples.min.js');
 	wp_enqueue_script( 'material-js', 'http://cdn.bootcss.com/bootstrap-material-design/0.2.0/js/material.min.js');
